024e2e31ff4522c1b51b28cc49480de05fb07adb
Aone <quanyiz@stanford.edu>
2018-08-29 14:05:16 -0700

    Aug 29

---
+++ GO_barplot.R
@@ -0,0 +1,49 @@
+#!/usr/bin/env Rscript
+#####################################
+# Usage:                            #
+# Manual:                           #
+#####################################
+library("ggplot2")
+options<-commandArgs(trailingOnly = T)
+plot_heigh<-4
+plot_width<-10
+png_name<-unlist(strsplit(options[1],'.',fixed=T))[1]
+png(file=paste(png_name,'png',sep='.'),height = plot_heigh, width = plot_width, res=600, units = "in", family="Arial")
+
+dat<-read.table(file=options[1],header=T,sep="\t")
+# keep original reversed order of $Term
+dat<- dat[seq(dim(dat)[1],1),]
+dat<-transform(dat,Term=factor(Term,levels=unique(Term)))
+
+#5FBBA3 brightgreen
+#4169A0 darkblue
+#C96486 pink
+
+pic<-ggplot(dat,aes(x=Term,y=-1*log(Pvalue),fill = group))+
+	geom_bar(stat= 'identity')+
+	#ploar bar
+	#coord_polar(theta = "x")+
+	#flip X-Y axis
+	coord_flip()+
+	scale_fill_manual(
+		values=c("#34888c","#7caa2d","#d09683")
+		)+
+	#theme_bw()+
+	theme(
+		legend.position=c(0.8,0.13),
+		legend.text = element_text(size=12), 
+		legend.title=element_blank(), 
+		axis.text.y=element_text(size=13,colour = 'black'),
+		axis.title.x = element_text(size=14,colour = 'black'),
+		axis.text.x =element_text(size=12,colour = 'black')
+		)+
+	xlab("")+
+	ylab("-log P-value")
+	
+pic
+dev.off()
+
+################ END ################
+#          Created by Aone          #
+#     quanyi.zhao@stanford.edu      #
+################ END ################
---
+++ GO_bubble_custom.R
@@ -0,0 +1,47 @@
+#!/usr/bin/env Rscript
+############################
+# Usage : ./GO_BP_bubble.R <BP.txt> 
+############################
+# arguments in command line
+options<-commandArgs(trailingOnly = T)
+library(ggplot2)
+library(stringr)
+plot_heigh<-2.2
+plot_width<-6
+
+my_data<-read.table(file=options[1],sep="\t")
+if(length(my_data[,1]) >= 10) {my_data<-my_data[1:10,]} else {
+my_data<-my_data}
+my_data$V1->Term
+my_data$V3->PValue
+my_data$V5->Fold.Enrichment
+my_data$V11->Count
+
+png_name<-unlist(strsplit(options[1],'.',fixed=T))[1]
+png(file=paste(png_name,'png',sep='.'),height = plot_heigh, width = plot_width, res=600, units = "in", family="Arial")
+
+ggplot(my_data, aes(x=-1*log10(PValue), y=Term)) + 
+	geom_point(aes(size=Count, color=Fold.Enrichment))+
+	scale_colour_gradient(low="steelblue", high="red")+
+	labs(
+		color=expression(Fold.Enrichment), 
+		size="Gene number",
+		x="-logP-value",
+		# y="Pathway name",
+		#title="Cellular Component"
+		#title="MSigDBPathway"
+		#title="PANTHER Pathway"
+		title="Biological Process")+
+	theme_bw()+
+	theme(
+		axis.text.y = element_text(size=rel(1.1)),
+		axis.text.x = element_text(size=rel(1.0)),
+		axis.title.x = element_text(size=rel(0.9)),
+		axis.title.y = element_blank(),
+		legend.key.size=unit(0.1,"inches"),
+		legend.text=element_text(size=7),
+		legend.title=element_text(size=8),
+		plot.title = element_text(hjust = 0.5,size=12)
+	)
+	
+dev.off()
