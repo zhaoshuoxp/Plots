17934007a747b0686a887c47caaae9b80020ee81
Aone <quanyiz@stanford.edu>
2019-09-26 14:08:50 -0700

    Update GO_BP_bubble.R

--- GO_BP_bubble.R
+++ GO_BP_bubble.R
@@ -7,7 +7,7 @@ options<-commandArgs(trailingOnly = T)
 library(Hmisc)
 library(stringr)
 plot_heigh<-2.5
-plot_width<-5.7
+plot_width<-5.3
 
 my_data<-read.table(file=options[1],header=T,sep="\t")
 if(length(my_data[,1]) >= 10) {my_data<-my_data[1:10,]} else {
@@ -17,7 +17,7 @@ my_data$Term<-str_split_fixed(my_data$Term,"~",2)[,2]
 png_name<-unlist(strsplit(options[1],'.',fixed=T))[1]
 png(file=paste(png_name,'png',sep='.'),height = plot_heigh, width = plot_width, res=600, units = "in", family="Arial")
 
-ggplot(my_data, aes(x=-1*log10(Pvalue), y=capitalize(Term))) + 
+ggplot(my_data, aes(x=-1*log10(PValue), y=capitalize(Term))) + 
 	geom_point(aes(size=Count, color=Fold.Enrichment))+
 	scale_colour_gradient(low="steelblue", high="red")+
 	labs(
@@ -28,7 +28,7 @@ ggplot(my_data, aes(x=-1*log10(Pvalue), y=capitalize(Term))) +
 		title="Biological Process")+
 	theme_bw()+
 	theme(
-		axis.text.y = element_text(size=rel(1.5),color='black'),
+		axis.text.y = element_text(size=rel(1.0),color='black'),
 		axis.text.x = element_text(size=rel(1.0)),
 		axis.title.x = element_text(size=rel(0.9)),
 		axis.title.y = element_blank(),
